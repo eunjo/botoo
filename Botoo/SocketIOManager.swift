ca9e541893776b4721aad358485c02de4507e8ab
이은조 <Eunjo@ieunjoui-MacBook-Pro.local>
2016-07-14 14:00:36 +0900

    no message

--- Botoo/SocketIOManager.swift
+++ Botoo/SocketIOManager.swift
@@ -18,30 +18,28 @@ class SocketIOManager: NSObject {
     
     var socket: SocketIOClient = SocketIOClient(socketURL: NSURL(string: "http://ec2-52-42-42-59.us-west-2.compute.amazonaws.com")!)
     
-    
+    // connecting
     func establishConnection() {
         socket.connect()
         print("connected successfully")
     }
-    /*
     
+    // disconnecting
     func closeConnection() {
         socket.disconnect()
     }
-     */
-    
-    /*
+
+    // app 활성화시 connect
     func applicationDidBecomeActive(application: UIApplication) {
         SocketIOManager.sharedInstance.establishConnection()
     }
-    */
     
-    /*
     
+    // app 비활성화시 disconnect
     func applicationDidEnterBackground(application: UIApplication) {
         SocketIOManager.sharedInstance.closeConnection()
     }
-    */
+    
     
 
 }
