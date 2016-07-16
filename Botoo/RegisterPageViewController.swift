583cdc16fe3f4ce9d1aed747f9e198bcf372b155
이은조 <Eunjo@ieunjoui-MacBook-Pro.local>
2016-07-16 16:44:05 +0900

    no message

--- Botoo/RegisterPageViewController.swift
+++ Botoo/RegisterPageViewController.swift
@@ -46,6 +46,8 @@ class RegisterPageViewController: UIViewController {
         let userPW = userPWTextField.text
         let PWrepeat = PWrepeatTextField.text
         
+        var isAlreadyExists:Bool?
+        
         // Check for Empty Fields
     
         if (userEmail!.isEmpty || userName!.isEmpty || userPW!.isEmpty || PWrepeat!.isEmpty || gender==nil){
@@ -64,19 +66,32 @@ class RegisterPageViewController: UIViewController {
             return
         }
         
+
+        
         // URL Info 객체 생성
         var urlInfoForRegister:URLInfo = URLInfo()
 
-        
         // Email 중복검사
-        let checkParams = [
-            "email": userEmail! as String,
-        ]
-        
-        urlInfoForRegister.test = urlInfoForRegister.WEB_SERVER_IP+"/checkEmail"
-        TestConstruct().testConnect(urlInfoForRegister, httpMethod: "GET", params: checkParams, completionHandler: { (json, error) -> Void in
+
+        urlInfoForRegister.test = urlInfoForRegister.WEB_SERVER_IP+"/checkEmail?email="+userEmail!
+        TestConstruct().testConnect(urlInfoForRegister, httpMethod: "GET", params: nil, completionHandler: { (json, error) -> Void in
             print("email 중복검사 :: \(json)")
+            isAlreadyExists = true
+            print(isAlreadyExists)
         })
+        /*
+        while(isAlreadyExists==nil){
+        }
+        */
+        
+        sleep(1)
+        print("if문 전")
+        if (isAlreadyExists==true){
+            print("if문 진입")
+            displayRegisterAlert("이미 가입된 이메일 주소입니다")
+            return
+        }
+        print("if문 후")
  
         
         
@@ -94,6 +109,8 @@ class RegisterPageViewController: UIViewController {
         
         TestConstruct().testConnect(urlInfoForRegister, httpMethod: "POST", params: loginParams as! Dictionary<String,String>, completionHandler: { (json, error) -> Void in
             print("가입 정보가 잘들어갔어욤 :: \(json)")
+            
+            
         })
         
         
@@ -103,7 +120,6 @@ class RegisterPageViewController: UIViewController {
         NSUserDefaults.standardUserDefaults().setObject(userPW, forKey: "userPW")
         NSUserDefaults.standardUserDefaults().synchronize()
         
-        
         // Display alert message with confirmation
         let myAlert = UIAlertController(title:"Alert", message: "Registration is successful", preferredStyle: UIAlertControllerStyle.Alert)
         
@@ -112,6 +128,7 @@ class RegisterPageViewController: UIViewController {
         }
         myAlert.addAction(okAction)
         self.presentViewController(myAlert, animated: true, completion: nil)
+        
         
     }
     
