074e361f50a9790312474943f8a64737ff857786
이은조 <Eunjo@ieunjoui-MacBook-Pro.local>
2016-07-16 21:25:34 +0900

    no message

--- Botoo/connectSceneViewController.swift
+++ Botoo/connectSceneViewController.swift
@@ -12,6 +12,15 @@ class connectSceneViewController: UIViewController {
 
     @IBOutlet weak var backButton: UIButton!
     
+    @IBOutlet weak var searchEmailTextField: UITextField!
+    @IBOutlet weak var searchResult: UILabel!
+    
+    var isGot:Bool?
+    
+    var loverEmailStored:String?
+    var loverGenderStored:String?
+    var loverNameStored:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,6 +39,41 @@ class connectSceneViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+
+    @IBAction func searchButtonTapped(sender: AnyObject) {
+        
+        let loverEmail:String = searchEmailTextField.text!
+        
+        // URL Info 객체 생성
+        var urlInfoForRegister:URLInfo = URLInfo()
+        
+        // 유저 정보 받아오기
+        urlInfoForRegister.test = urlInfoForRegister.WEB_SERVER_IP+"/checkEmail?email="+loverEmail
+        TestConstruct().testConnect(urlInfoForRegister, httpMethod: "GET", params: nil, completionHandler: { (json, error) -> Void in
+            self.isGot = true
+            self.loverEmailStored = String(json["email"])
+            self.loverNameStored = String(json["name"])
+            
+            self.loverEmailStored = self.loverEmailStored!.stringByReplacingOccurrencesOfString("Optional(", withString: "")
+            self.loverEmailStored = self.loverEmailStored!.stringByReplacingOccurrencesOfString(")", withString: "")
+            
+            self.loverNameStored = self.loverNameStored!.stringByReplacingOccurrencesOfString("Optional(", withString: "")
+            self.loverNameStored = self.loverNameStored!.stringByReplacingOccurrencesOfString(")", withString: "")
+            
+        })
+        
+        sleep(1)
+        searchResult.text = loverNameStored
+        
+        print("등록안된 이메일일때")
+        searchResult.text = "존재하지 않는 사용자입니다"
+        return
+        
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
