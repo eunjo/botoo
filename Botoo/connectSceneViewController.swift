552b846024007cf6e9d980d7de309cddd218a45b
jjoon92 <seoljunmyung@seoljunmyeong-ui-MacBook-Pro.local>
2016-07-20 13:31:34 +0900

    no message

--- Botoo/connectSceneViewController.swift
+++ Botoo/connectSceneViewController.swift
@@ -51,27 +51,35 @@ class connectSceneViewController: UIViewController {
         
         let loverEmail = searchEmailTextField.text
         
-        MemberConstruct().checkEmail(loverEmail!, completionHandler: { (json, error) -> Void in
-            if json != nil {
-                self.isGot = true
-                
-                self.loverEmailStored = json["email"] as? String
-                self.loverNameStored = json["name"] as? String
-                
-                self.connectButton.enabled = true
-            }
-            self.threadIsAlive = 1
-        })
-        
-        while self.threadIsAlive == 0 {}
-        
-        searchResult.text = loverNameStored
-        
-        if !isGot {
-            print("등록안된 이메일일때")
-            searchResult.text = "존재하지 않는 사용자입니다"
+        if NSUserDefaults.standardUserDefaults().stringForKey("userEmail")==loverEmail{
+            
+            searchResult.text = "자기 자신입니다"
             connectButton.enabled = false
             return
+        } else {
+            MemberConstruct().checkEmail(loverEmail!, completionHandler: { (json, error) -> Void in
+                if json != nil {
+                    self.isGot = true
+                    
+                    self.loverEmailStored = json["email"] as? String
+                    self.loverNameStored = json["name"] as? String
+                    
+                    self.connectButton.enabled = true
+                }
+                self.threadIsAlive = 1
+            })
+            
+            while self.threadIsAlive == 0 {}
+            
+            searchResult.text = loverNameStored
+            
+            
+            if !isGot {
+                print("등록안된 이메일일때")
+                searchResult.text = "존재하지 않는 사용자입니다"
+                connectButton.enabled = false
+                return
+            }
         }
     }
     
