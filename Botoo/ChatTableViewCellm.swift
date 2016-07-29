90c345dbde6d4cea980228a31898d676aa3dfc5e
이은조 <Eunjo@ieunjoui-MacBook-Pro.local>
2016-07-29 14:22:43 +0900

    ㅇㄹ

--- Botoo/ChatTableViewCellm.swift
+++ Botoo/ChatTableViewCellm.swift
@@ -22,8 +22,8 @@ class ChatTableViewCellm: UITableViewCell {
         let myImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: messageBubble.frame.width, height: messageBubble.frame.height))
         myImageView.image = myImage
         messageBubble.addSubview(myImageView)
+
         
-    //    messageBubble.backgroundColor = UIColor(patternImage: UIImage(named: "chatBubble.png")!)
     }
     
     override func setSelected(selected: Bool, animated: Bool) {
